bd7f4dc2bddd1e787c455e1cdb844adefc925ab3
Abderrahim Ajakka <ajakka.abderrahim@gmail.com>
2024-07-18 17:30:10 +0100

    cleanup example code

--- example/src/features/routes/RoutesScreen.tsx
+++ example/src/features/routes/RoutesScreen.tsx
@@ -1,75 +1,33 @@
-import { useState, useEffect } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
 import type { GeoCoordinates, GeoPolyline } from 'react-native-here-explore';
-import {
-  Map,
-  Marker,
-  Polyline,
-  RouteOption,
-  useRouting,
-} from 'react-native-here-explore';
-
-import { BicycleSVG, BusSVG, CarSVG, TruckSVG } from '@/components/icons';
-import { ScooterSVG, TaxiSVG, PedestrianSVG } from '@/components/icons';
-import RoundButton from '@/components/RoundButton';
+import { Map, Marker, Polyline } from 'react-native-here-explore';
+import { useRouting, RouteOption } from 'react-native-here-explore';
 
 import type { ScreenNames, ScreenProps } from '@/navigation';
+import RouteOptionsSelector from './components/RouteOptionsSelector';
+
+export const RoutesScreenName: ScreenNames = 'Routes';
+
+const centerPoint: GeoCoordinates = {
+  latitude: 52.51967475,
+  longitude: 13.36895715,
+};
 
 const wayPoints: GeoCoordinates[] = [
   { latitude: 52.5561936, longitude: 13.3432207 },
   { latitude: 52.4841669, longitude: 13.3957046 },
 ];
 
-const routeOptions = [
-  {
-    title: 'Pedestrian',
-    image: <PedestrianSVG color="white" />,
-    routeOption: RouteOption.pedestrian(),
-  },
-  {
-    title: 'Bicycle',
-    image: <BicycleSVG color="white" />,
-    routeOption: RouteOption.bicycle(),
-  },
-  {
-    title: 'Scooter',
-    image: <ScooterSVG color="white" />,
-    routeOption: RouteOption.scooter(),
-  },
-  {
-    title: 'Car',
-    image: <CarSVG color="white" />,
-    routeOption: RouteOption.car(),
-  },
-  {
-    title: 'Bus',
-    image: <BusSVG color="white" />,
-    routeOption: RouteOption.bus(),
-  },
-  {
-    title: 'Taxi',
-    image: <TaxiSVG color="white" />,
-    routeOption: RouteOption.taxi(),
-  },
-  {
-    title: 'Truck',
-    image: <TruckSVG color="white" />,
-    routeOption: RouteOption.truck(),
-  },
-];
-
-export const RoutesScreenName: ScreenNames = 'Routes';
-
-export default function RoutesScreen(props: ScreenProps<'Routes'>) {
-  const { bottom } = useSafeAreaInsets();
-
-  const [vertices, setVertices] = useState<GeoPolyline>([]);
-  const [route, setRoute] = useState<RouteOption>(RouteOption.pedestrian());
+export default function RoutesScreen(_: ScreenProps<'Routes'>) {
+  const [vertices, setVertices] = React.useState<GeoPolyline>([]);
+  const [route, setRoute] = React.useState<RouteOption>(
+    RouteOption.pedestrian()
+  );
 
   const { cancel, calculateRoute } = useRouting();
-  useEffect(() => {
+  React.useEffect(() => {
     runCalculateRoute();
     return () => void cancel();
   }, [route]);
@@ -86,21 +44,17 @@ export default function RoutesScreen(props: ScreenProps<'Routes'>) {
       <Map
         style={styles.box}
         mapScheme="NORMAL_NIGHT"
-        geoCoordinates={{ latitude: 52.51967475, longitude: 13.36895715 }}
+        geoCoordinates={centerPoint}
         zoomValue={13.4}
       >
-        <Marker
-          geoCoordinates={wayPoints[0]!}
-          size={{ width: 128, height: 128 }}
-          anchor={{ vertical: 0.9 }}
-          image={require('@/assets/marker.png')}
-        />
-        <Marker
-          geoCoordinates={wayPoints[1]!}
-          size={{ width: 128, height: 128 }}
-          anchor={{ vertical: 0.9 }}
-          image={require('@/assets/marker.png')}
-        />
+        {wayPoints.map((wayPoint) => (
+          <Marker
+            geoCoordinates={wayPoint}
+            size={{ width: 128, height: 128 }}
+            anchor={{ vertical: 0.9 }}
+            image={require('@/assets/marker.png')}
+          />
+        ))}
         <Polyline
           lineType="SOLID"
           lineColor="#72A1F1"
@@ -109,24 +63,10 @@ export default function RoutesScreen(props: ScreenProps<'Routes'>) {
         />
       </Map>
 
-      <View style={[styles.bottomSheet, { paddingBottom: bottom }]}>
-        <Text style={styles.title}>Route Options</Text>
-        <ScrollView
-          horizontal={true}
-          style={styles.scrollView}
-          contentContainerStyle={{ paddingHorizontal: 16 }}
-        >
-          {routeOptions.map(({ title, image, routeOption }) => (
-            <RoundButton
-              key={title}
-              title={title}
-              image={image}
-              onPress={() => setRoute(routeOption)}
-              selected={route.equals(routeOption)}
-            />
-          ))}
-        </ScrollView>
-      </View>
+      <RouteOptionsSelector
+        selectedRoute={route}
+        onRouteOptionPress={setRoute}
+      />
     </View>
   );
 }
---
+++ example/src/features/routes/components/RouteOptionsSelector.tsx
@@ -0,0 +1,98 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { BicycleSVG, BusSVG, CarSVG, TruckSVG } from '@/components/icons';
+import { ScooterSVG, TaxiSVG, PedestrianSVG } from '@/components/icons';
+import RoundButton from '@/components/RoundButton';
+import { RouteOption } from 'react-native-here-explore';
+
+const RouteOptions = [
+  {
+    title: 'Pedestrian',
+    image: <PedestrianSVG color="white" />,
+    routeOption: RouteOption.pedestrian(),
+  },
+  {
+    title: 'Bicycle',
+    image: <BicycleSVG color="white" />,
+    routeOption: RouteOption.bicycle(),
+  },
+  {
+    title: 'Scooter',
+    image: <ScooterSVG color="white" />,
+    routeOption: RouteOption.scooter(),
+  },
+  {
+    title: 'Car',
+    image: <CarSVG color="white" />,
+    routeOption: RouteOption.car(),
+  },
+  {
+    title: 'Bus',
+    image: <BusSVG color="white" />,
+    routeOption: RouteOption.bus(),
+  },
+  {
+    title: 'Taxi',
+    image: <TaxiSVG color="white" />,
+    routeOption: RouteOption.taxi(),
+  },
+  {
+    title: 'Truck',
+    image: <TruckSVG color="white" />,
+    routeOption: RouteOption.truck(),
+  },
+];
+
+export type RouteOptionsSelectorProps = {
+  selectedRoute: RouteOption;
+  onRouteOptionPress: (routeOption: RouteOption) => void;
+};
+
+export default function RouteOptionsSelector({
+  selectedRoute,
+  onRouteOptionPress,
+}: RouteOptionsSelectorProps) {
+  const { bottom } = useSafeAreaInsets();
+
+  return (
+    <View style={[styles.bottomSheet, { paddingBottom: bottom }]}>
+      <Text style={styles.title}>Route Options</Text>
+      <ScrollView
+        horizontal={true}
+        style={styles.scrollView}
+        contentContainerStyle={{ paddingHorizontal: 16 }}
+      >
+        {RouteOptions.map(({ title, image, routeOption }) => (
+          <RoundButton
+            key={title}
+            title={title}
+            image={image}
+            onPress={() => onRouteOptionPress(routeOption)}
+            selected={selectedRoute.equals(routeOption)}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  bottomSheet: {
+    backgroundColor: '#292F3A',
+    borderTopStartRadius: 4,
+    borderTopEndRadius: 4,
+  },
+  title: {
+    marginHorizontal: 16,
+    marginVertical: 16,
+    color: 'white',
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  scrollView: {
+    width: '100%',
+    alignContent: 'center',
+    marginBottom: 16,
+  },
+});
